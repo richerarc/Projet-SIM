c717fd3b4a0e0dadf04c9ec84a1de50f7df663e0
JulienDuf <dufresne_julien@hotmail.ca>
2015-02-26 11:56:28 -0500

    Ajout de la classe vecteur

---
+++ sources/Vecteur.h
@@ -0,0 +1,90 @@
+
+template <class T>
+
+class Vecteur {
+
+public:
+
+	T composanteX, 
+	composanteY,
+	composanteZ;
+
+	T calculerNorme() {
+
+		T composantesCarre = 0;
+
+		composantesCarre += pow(composanteX, 2);
+		composantesCarre += pow(composanteY, 2);
+		composantesCarre += pow(composanteZ, 2);
+
+		return sqrt(composantesCarre);
+	}
+
+	Vecteur(T compX, T compY, T compZ) {
+
+		composanteX = compX;
+		composanteY = compY;
+		composanteZ = compZ;
+		
+	}
+
+	void normaliserVecteur() {
+
+		T norme = calculerNorme();
+
+		composanteX /= norme;
+		composanteY /= norme;
+		composanteZ /= norme;
+	}
+
+	void ajouterVecteur(CVecteur<T>& vecteur) {
+
+		
+		composanteX += vecteur.composanteX;
+		composanteY += vecteur.composanteY;
+		composanteZ += vecteur.composanteZ;
+	}
+
+	void soustraireVecteur(CVecteur<T>& vecteur) {
+
+		composanteX -= vecteur.composanteX;
+		composanteY -= vecteur.composanteY;
+		composanteZ -= vecteur.composanteZ;
+
+	}
+
+	void produitParScalaire(double scalaire) {
+
+		composanteX *= scalaire;
+		composanteY *= scalaire;
+		composanteZ *= scalaire;
+	}
+
+	static CVecteur<T>* somme(CVecteur<T>& vecteur1, CVecteur<T>& vecteur2) {
+
+		if (vecteur1.ObtenirNbrComposante() == vecteur2.ObtenirNbrComposante() && vecteur1.ObtenirNbrComposante() == 3)
+			return new CVecteur<T>(m_NbrComposantes, vecteur1.composanteX + vecteur2.composanteX, vecteur1.composanteY + vecteur2.composanteY, vecteur1.composanteZ + vecteur2.composanteZ);
+
+	}
+
+	static CVecteur<T>* difference(CVecteur<T>& vecteur1, CVecteur<T>& vecteur2) {
+
+		if (vecteur1.ObtenirNbrComposante() == vecteur2.ObtenirNbrComposante() && vecteur1.ObtenirNbrComposante() == 3)
+			return new CVecteur<T>(vecteur1.ObtenirNbrComposante(), vecteur1.composanteX - vecteur2.composanteX, vecteur1.composanteY - vecteur2.composanteY, vecteur1.composanteZ - vecteur2.composanteZ);
+
+	}
+
+	static CVecteur<T>* produitVectorielle(CVecteur<T>& vecteur1, CVecteur<T>& vecteur2) {
+
+		if (vecteur1.ObtenirNbrComposante() == vecteur2.ObtenirNbrComposante() && vecteur1.ObtenirNbrComposante() == 3)
+			return new CVecteur<T>(vecteur1.ObtenirNbrComposante(), (vecteur1.composanteY * vecteur2.composanteZ) - (vecteur2.composanteZ * vecteur1.composanteY), (vecteur1.composanteZ * vecteur2.composanteX) - (vecteur2.composanteZ * vecteur1.composanteX), (vecteur1.composanteX * vecteur2.composanteY) - (vecteur2.composanteX * vecteur1.composanteY));
+	}
+
+	static double produitScalaire(CVecteur<T>& vecteur1, CVecteur<T>& vecteur2) {
+
+		if (vecteur1.ObtenirNbrComposante() == vecteur2.ObtenirNbrComposante() && vecteur1.ObtenirNbrComposante() == 3)
+			return ((vecteur1.composanteX * vecteur2.composanteX) + (vecteur1.composanteY * vecteur2.composanteY) + (vecteur1.composanteZ * vecteur2.composanteZ));
+
+	}
+
+};
