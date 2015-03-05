df186cd4590dd3c4d8900359bf831921218ad52e
Kevin Pantelakis <kev.pantelakis@gmail.com>
2015-03-05 08:52:47 -0500

    Ajustements

--- sources/Model3D.h
+++ sources/Model3D.h
@@ -1,31 +1,37 @@
 #pragma once
 #include "Model.h"
 #include "Texture.h"
+#include <vector>
 
 class Model3D{
 private:
 	Model model;
 	Texture texture;
 	Vecteur3f echelle;
-	float* boiteDeCollision;
+	float boiteDeCollision[8];
 	void calculerBoiteDeCollision(){};
 
 public:
 
 	Model3D(){}
 
+	Model3D(Model &model, Texture &texture){
+		this->model = model;
+		this->texture = texture;
+	}
+
 	~Model3D(){}
 
-	void defModel(Model &modeltmp){
-		model = modeltmp;
+	void defModel(Model &model){
+		this->model = modeltmp;
 	}
 
-	void defTexture(Texture &Texturetmp){
-		texture = Texturetmp;
+	void defTexture(Texture &texture){
+		this->texture = Texture;
 	}
 
-	void defEchelle(Vecteur3f &Echelletmp){
-		echelle = Echelletmp;
+	void defEchelle(Vecteur3f &echelle){
+		this->echelle = echelle;
 	}
 
 	Model obtModel(){
