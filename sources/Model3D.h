2a4882766f3a5e699ee2945a994831012fc8990e
Diego Manrique <dipavit@hotmail.com>
2015-03-05 11:37:34 -0500

    Calcul de la Boite de collision dans Model3D par Diego

--- sources/Model3D.h
+++ sources/Model3D.h
@@ -9,7 +9,40 @@ private:
 	Texture texture;
 	Vecteur3f echelle;
 	float boiteDeCollision[8];
-	void calculerBoiteDeCollision(){};
+	void calculerBoiteDeCollision(){
+		std::vector<float> tmpX;
+		std::vector<float> tmpY;
+		std::vector<float> tmpZ;
+		float xmax, xmin, ymax, ymin, zmax, zmin;
+		for (int i = 0; i < nbrFaces; ++i){
+			tmpX.push_back(modele->obtVertices()[i * 3]);
+			tmpY.push_back(modele->obtVertices()[i * 3 + 1]);
+			tmpZ.push_back(modele->obtVertices()[i * 3 + 2]);
+		}
+		xmax = Maths::obtValeurMax(tmpX);
+		xmin = Maths::obtValeurMin(tmpX);
+		ymax = Maths::obtValeurMax(tmpY);
+		ymin = Maths::obtValeurMin(tmpY);
+		zmax = Maths::obtValeurMax(tmpZ);
+		zmin = Maths::obtValeurMin(tmpZ);
+
+		for (int i = 0, i < 24, i++){
+			switch (i % 3){
+			case 0:
+				if (i < 12) boiteDeCollision[i] = xmax;
+				else boiteDeCollision[i] = xmin;
+				break;
+			case 1:
+				if ((i % 12) < 6) boiteDeCollision[i] = ymax;
+				else boitDeCollision[i] = ymin;
+				break;
+			case 2:
+				if ((i % 6) < 3) boiteDeCollision[i] = zmax;
+				else boitDeCollision[i] = zmin;
+				break;
+			}
+		}
+	}
 
 public:
 
