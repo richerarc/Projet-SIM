ea8d5eb7a78be241468da106b5e96494b16f25a9
richerarc <richer.arc@gmail.com>
2015-03-05 08:44:59 -0500

    ajustememnt

--- sources/GestionnaireLiens.h
+++ sources/GestionnaireLiens.h
@@ -6,31 +6,34 @@
 #include "Porte.h"
 #include "Salle.h"
 #include "Fabrique.h"
-class GestionnaireLens : public Singleton<GestionnaireLens>{
+class GestionnaireLiens : public Singleton<GestionnaireLiens>{
 private:
 	std::map<std::tuple<unsigned int, unsigned int, bool>, std::tuple<unsigned int, unsigned int>> connections;
 	std::list<Salle> salles;
 	std::list<Porte> portes;
 public:
-	GestionnaireLens(){}
-	~GestionnaireLens(){}
+	GestionnaireLiens(){}
+	~GestionnaireLiens(){}
 
 	void ajouter(std::tuple<unsigned int, unsigned int, bool> entree, std::tuple<unsigned int, unsigned int> sortie){
 		connections[entree] = sortie;
 	}
 	std::tuple<unsigned int, unsigned int> destination(std::tuple<unsigned int, unsigned int, bool> sortie){
-		return connections.find(sortie)->second;
+			//return connections.find(sortie)->second;
+		return connections[sortie];
 	}
 	Porte obtPorte(unsigned int _ID){
 		for (auto it : portes){
 			if (it.obtID() == _ID)
 				return it;
 		}
+		return portes.front();
 	}
 	Salle obtSalle(unsigned int _ID){
 		for (auto it : salles){
 			if (it.obtID() == _ID)
 				return it;
 		}
+		return salles.front();
 	}
 };
