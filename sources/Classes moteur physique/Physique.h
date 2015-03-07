157ec15fb1a0042aed428ad38e4641e4f8ac7383
JulienDuf <dufresne_julien@hotmail.ca>
2015-03-06 21:11:58 -0500

    Première version de la fonction position point droite

--- sources/Classes moteur physique/Physique.h
+++ sources/Classes moteur physique/Physique.h
@@ -16,17 +16,30 @@ public:
 	Physique() {
 		temps.partir();
 		constanteDeFriction = 0.5;
+		gravite = 9.8;
+	}
+
+	Vecteur3d obtenirNormaleSurface(Vecteur3d& position, double rayon) {
+
+	}
+
+	double obtenirForceNormale(double masse, Vecteur3d& position) {
+
 	}
 
 	void appliquerGravite(Vecteur3d& vecteurVitesseObjet) {
 		vecteurVitesseObjet.y -= gravite;
+	}
+
+	void appliquerVent(Vecteur3d& VecteurVitesseObjet) {
+	
 	}
 	
 	void appliquerFrottement(Objet& objet) {
 		objet.vecteurVitesse -= constanteDeFriction * obtenirForceNormale(objet.masse, objet.vecteurPosition);
 	}
 	
-	double obtenirAnglePenduleSimple(double& angleMaximal, double& omega) {
+	double obtenirAnglePenduleSimple(double angleMaximal, double omega) {
 		return angleMaximal * SDL_cos(omega * temps.obtenirTemps());
 	}
 
@@ -35,11 +48,15 @@ public:
 		return SDL_sqrt(SDL_pow((point2.x - point1.x), 2) + SDL_pow((point2.y - point1.y), 2) + SDL_pow((point2.z - point1.z), 2));
 	}
 
-	double obtenirEnergieCinetique(double& masse, Vecteur3d& vecteurVitesseObjet) {
+	int positionPointDroite(Vecteur3d& droite1, Vecteur3d& droite2, Vecteur3d& point) {
+		
+		Vecteur3d VectTmp = droite1 - droite2;
+		Vecteur3d VectRes(-Vect.y, Vect.x, Vect.z);
+		return VectRes(point - droite1);
+			
+	}
+
+	double obtenirEnergieCinetique(double masse, Vecteur3d& vecteurVitesseObjet) {
 		return 0.5 * masse * SDL_pow(vecteurVitesseObjet.norme(), 2);
-	}
-
-	double obtenirForceNormale(double& masse, Vecteur3d& position) {
-
-	}
+	}	
 };
