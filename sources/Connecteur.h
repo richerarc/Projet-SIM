9bb174f210dc6b287a8d0c270a5ee419ac07621a
Richer Archambault <richer.arc@gmail.com>
2015-03-09 07:07:14 -0400

    Update Connecteur.h

--- sources/Connecteur.h
+++ sources/Connecteur.h
@@ -1,9 +1,12 @@
-class Connecteur : public Salle{
+#pragma once
+#include "Salle.h"		
+class Connecteur : public Salle(){		
 private:
-
+ 		 
 public:
-	Connecteur(){
-
-	}
-
+	Connecteur(){}
+	
+	Connecteur(Vecteur3d Pos, Modele3D modele) : public Salle(Pos, modele){}
+ 		 
+	
 };
