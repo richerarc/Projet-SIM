aa6e46d2a7c4c468d18b09ce8027af59b0cdc68d
richerarc <richer.arc@gmail.com>
2015-03-11 13:22:41 -0400

    Finalisation de la classe Fabrique. Closed #7
    À noter que l'acces au modeles et au texture est provisoire, étant donner que le gestionnaire de ressource est encore inexistant. @SirKhepre

--- sources/Fabrique.h
+++ sources/Fabrique.h
@@ -1,23 +1,32 @@
 #pragma once
 
-enum salle_t {PIECE_RONDE_PUZ1, PIECE_RONDE_PUZ2, PIECE_RONDE_PUZ3, PIECE_RONDE_PUZ4, PIECE_RECTANGLE_PUZ1, PIECE_RECTANGLE_PUZ2, PIECE_RECTANGLE_PUZ3, PIECE_RECTANGLE_PUZ4};
-
 #ifndef Fabrique_Fabrique_h
 #define Fabrique_Fabrique_h
 
-	//Manque la classe model avant de la tester
-//class Fabrique : public Singleton<Fabrique> {
-//public:
-//	Model3d obtSalle(salle_t type){
-//		switch (type) {
-//			case PIECE_RONDE_PUZ1:
-//				return NULL;
-//				break;
-//				
-//			default:
-//    break;
-//		}
-//	}
-//};
+Manque la classe model avant de la tester
+class Fabrique : public Singleton<Fabrique> {
+private:
+	static int pos,
+	itterateurID,
+	nbrModelPiece,
+	nbrModelConnecteur,
+	nbrTexturePiece;
+public:
+	Salle obtSalle(bool typeSalle){ // true pour une pièce et false pour un connecteur
+		switch (type) {
+			case false:
+				return Connecteur(Modele3D(GestionnaireRessources::obtInstance().obtModel(rand() % nbrModelConnecteur + 15), GestionnaireRessources::obtInstance().obtTexture(rand() % nbrTexturePiece)), ++this->itterateurID);
+				break;
+			default:
+				return Piece(Modele3D(GestionnaireRessources::obtInstance().obtModel(rand() % nbrModelPiece), GestionnaireRessources::obtInstance().obtTexture(rand() % nbrTexturePiece)), ++this->itterateurID);
+				break;
+		}
+	}
+};
 
+int Fabrique::pos = -100;
+int Fabrique::itterateurID = 0;
+int Fabrique::nbrModelPiece = 15;
+int Fabrique::nbrModelConnecteur = 8;
+int Fabrique::nbrTexturePiece = 6;
 #endif
