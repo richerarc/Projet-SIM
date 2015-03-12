51529080529792fae7cca834ee651ca4895fd652
Nicolas Coutu <nico.coutu@hotmail.com>
2015-03-12 11:53:09 -0400

    Création de petits graphes pour la carte #18

---
+++ sources/Graphe.h
@@ -0,0 +1,12 @@
+namespace graphe{
+	int arbrePont1[1][1] = { 3 };
+	int arbrePont2a[2][2] = {0,1,1,1};
+	int arbrePont2b[2][2] = { 0, 2, 2, 0 };
+	int arbrePont3[3][3] = { 0, 1, 1, 0, 0, 1, 1, 0, 0 };
+	int arbrePont4a[4][4] = { 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0 };
+	int arbrePont4b[4][4] = { 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 0, 0 };
+	int arbrePont5a[5][5] = { 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0 };
+	int arbrePont5b[5][5] = { 0, 0, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0 };
+	int arbrePont5c[5][5] = { 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0 };
+	int* arbresPonts[9] = { arbrePont1[0], arbrePont2a[0], arbrePont2b[0], arbrePont3[0], arbrePont4a[0], arbrePont4b[0], arbrePont5a[0], arbrePont5b[0], arbrePont5c[0] };
+}
