84c47452b7ead262d046fd72bd40c1de5e1bcab2
Kevin Pantelakis <kev.pantelakis@gmail.com>
2015-03-12 20:24:23 -0400

    textures.h finie

--- sources/Classes Moteur Graphique/Texture.h
+++ sources/Classes Moteur Graphique/Texture.h
@@ -1,30 +1,47 @@
 #pragma once
+#include "Vecteur2.h"
+
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_opengl.h>
 #include <SDL2/SDL_image.h>
-#include "Vecteur2.h"
-class Texture{
-public:
-	SDL_Surface* surface;
+namespace gfx{
+	class Texture{
+	private:
+		SDL_Surface* surface;
+		GLuint ID;
+	public:
 
-	Texture(){
-		surface = nullptr;
-	}
-	~Texture(){
-		if (surface != nullptr){
-			SDL_FreeSurface(surface);
+		Texture(){
 			surface = nullptr;
 		}
-	}
-	bool charger(const char* path){
-		surface = SDL_LoadBMP(path);
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, surface->w, surface->h, 0, GL_BGR, GL_UNSIGNED_BYTE, surface->pixels);
-		return (surface != NULL);
-	}
-	Vecteur2ui obtTaille(){
-		return Vecteur2ui(surface->w, surface->h);
-	}
-	SDL_Surface* obtSurface(){
-		return surface;
-	}
-};
+
+		~Texture(){
+			if (surface != nullptr){
+				SDL_FreeSurface(surface);
+				surface = nullptr;
+			}
+		}
+
+		bool charger(const char* path){
+			surface = IMG_Load(path);
+			glGenTextures(1, &ID);
+			glBindTexture(GL_TEXTURE_2D, ID);
+			glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, surface->w, surface->w, 0, GL_BGR, GL_UNSIGNED_BYTE, surface->pixels);
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+			return (surface != NULL);
+		}
+
+		GLuint obtID(){
+			return ID;
+		}
+
+		Vecteur2ui obtTaille(){
+			return Vecteur2ui(surface->w, surface->h);
+		}
+
+		SDL_Surface* obtSurface(){
+			return surface;
+		}
+	};
+}
