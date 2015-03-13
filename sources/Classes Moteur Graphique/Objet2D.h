8b3388c6fe19b746082440f381d7aa66c6690015
Jessy Desjardins <superjess_2@hotmail.com>
2015-03-12 20:20:28 -0400

    afficher

--- sources/Classes Moteur Graphique/Objet2D.h
+++ sources/Classes Moteur Graphique/Objet2D.h
@@ -12,7 +12,40 @@ public:
 		this->position = position;
 	}
 	void afficher(){
-		glPushAttrib(GL_DEPTH_BUFFER_BIT | GL_LIGHTING_BIT);
+
+
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		glDisable(GL_DEPTH_TEST);
+		glMatrixMode(GL_PROJECTION);
+		glLoadIdentity();
+		glOrtho(0, 500, 0, 500, -1, 1);
+		glMatrixMode(GL_MODELVIEW);
+		glPushMatrix();
+		glLoadIdentity();
+
+		glEnable(GL_TEXTURE_2D);
+		unsigned int ID;
+		glGenTextures(1, &ID);
+		glBindTexture(GL_TEXTURE_2D, ID);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		glBegin(GL_QUADS);
+
+		glTexCoord2i(0, 0);
+		glVertex2i(0, 0);
+		glTexCoord2i(1,0);
+		glVertex2i(surface->w, 0);
+		glTexCoord2i(1, 1);
+		glVertex2i(surface->w,surface->h);
+		glTexCoord2i(0, 1);
+		glVertex2i(0,surface->h);
+		glEnd();
+		glPopMatrix();
+
+
+
+
+	/*	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		glDisable(GL_DEPTH_TEST);
 		glDisable(GL_LIGHTING);
 		glMatrixMode(GL_PROJECTION);
@@ -25,7 +58,12 @@ public:
 		glBindTexture(GL_TEXTURE_2D, ID);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);//Réduire la texture
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);//Agrandire la texture
+
+		
+
+		glEnableClientState(GL_TEXTURE_COORD_ARRAY);
 		glBegin(GL_QUADS);
+
 		glTexCoord2i(0,surface->h);
 		glVertex2i(0, 0);
 		glTexCoord2i(surface->w, surface->h);
@@ -34,8 +72,11 @@ public:
 		glVertex2i(surface->w, surface->h);
 		glTexCoord2i(0, 0);
 		glVertex2i(0, surface->h);
+
 		glDisableClientState(GL_TEXTURE_COORD_ARRAY);
 		glEnd();
+		glFlush();
+		*/
 	}
 
 	void defPosition(Vecteur3f position){
