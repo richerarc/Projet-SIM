7925d56eb9c8dc9f48795d76e7577a8e3fd7ab43
Jessy Desjardins <superjess_2@hotmail.com>
2015-03-15 18:30:00 -0400

    Le afficher avance encore :+1:

--- sources/Classes Moteur Graphique/Objet2D.h
+++ sources/Classes Moteur Graphique/Objet2D.h
@@ -1,95 +1,92 @@
 #pragma once
 #include "Vecteur3.h"
+#include "Vecteur2.h"
 class Objet2D{
 protected:
 	Vecteur3f position;
+	float positionx;
+	float positiony;
 	Vecteur3f orientation;
 	Vecteur3f origine;
-	Vecteur3i echelle;
+	Vecteur2i echelle;
+	float echelled;
 	SDL_Surface* surface;
 public:
 	Objet2D(Vecteur3f position){
-		this->position = position;
+		//this->position = position;
+		//Setter l'origine à la position, et calculer la nouvelle position résultante
+	//	origine = this->position;
+		//this->position.x = position.x - (surface->w / 2);
+	//	this->position.y = position.y - (surface->h / 2);
+
+	}
+	~Objet2D(){
+		if (surface != nullptr){
+			SDL_FreeSurface(surface);
+			surface = nullptr;
+		}
 	}
 	void afficher(){
-
-
+		
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		glDisable(GL_DEPTH_TEST);
 		glMatrixMode(GL_PROJECTION);
 		glLoadIdentity();
-		glOrtho(0, 500, 0, 500, -1, 1);
+		glOrtho(0, 500, 0, 500, -1, 1); //Remplacer les deux 500 par la windows
 		glMatrixMode(GL_MODELVIEW);
 		glPushMatrix();
 		glLoadIdentity();
-
 		glEnable(GL_TEXTURE_2D);
-		unsigned int ID;
-		glGenTextures(1, &ID);
-		glBindTexture(GL_TEXTURE_2D, ID);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		
 		glBegin(GL_QUADS);
+		//Mettre la surface à l'echelle
+		//surface->h = surface->h*echelled;
+		//surface->w = surface->w*echelled;
 
 		glTexCoord2i(0, 0);
-		glVertex2i(0, 0);
-		glTexCoord2i(1,0);
-		glVertex2i(surface->w, 0);
+		glVertex2i(positionx, positiony);  //1
+
+		glTexCoord2i(1, 0);
+		glVertex2i(surface->w, positiony); //2
+
 		glTexCoord2i(1, 1);
-		glVertex2i(surface->w,surface->h);
+		glVertex2i(surface->w, surface->h); //3
+
 		glTexCoord2i(0, 1);
-		glVertex2i(0,surface->h);
+		glVertex2i(positionx, surface->h); //4
+	/*	glTexCoord2i(0, 0);
+		glVertex2i(position.x, position.y);  //1
+
+		glTexCoord2i(1,0);
+		glVertex2i(surface->w, position.y); //2
+
+		glTexCoord2i(1, 1);
+		glVertex2i(surface->w, surface->h); //3
+
+		glTexCoord2i(0, 1);
+		glVertex2i(position.x, surface->h); //4*/
+
 		glEnd();
 		glPopMatrix();
-
-
-
-
-	/*	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-		glDisable(GL_DEPTH_TEST);
-		glDisable(GL_LIGHTING);
-		glMatrixMode(GL_PROJECTION);
-		glLoadIdentity();
-		glOrtho(0, 500, 0, 500, -1, 1);
-		glMatrixMode(GL_MODELVIEW);
-		glEnable(GL_TEXTURE_2D);
-		unsigned int ID;
-		glGenTextures(1, &ID);
-		glBindTexture(GL_TEXTURE_2D, ID);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);//Réduire la texture
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);//Agrandire la texture
-
-		
-
-		glEnableClientState(GL_TEXTURE_COORD_ARRAY);
-		glBegin(GL_QUADS);
-
-		glTexCoord2i(0,surface->h);
-		glVertex2i(0, 0);
-		glTexCoord2i(surface->w, surface->h);
-		glVertex2i(surface->w, 0);
-		glTexCoord2i(surface->w, 0);
-		glVertex2i(surface->w, surface->h);
-		glTexCoord2i(0, 0);
-		glVertex2i(0, surface->h);
-
-		glDisableClientState(GL_TEXTURE_COORD_ARRAY);
-		glEnd();
-		glFlush();
-		*/
 	}
 
-	void defPosition(Vecteur3f position){
-		this->position = position;
+	void defPosition(float positionx, float positiony){
+		this->positionx = positionx - (surface->w / 2);
+		this->positiony = positiony - (surface->h / 2);
+		//this->position = position;
+	//	this->position.x = position.x - (surface->w / 2);
+		//this->position.y = position.y - (surface->h / 2);
 	}
 	void defOrigine(Vecteur3f origine){
 		this->origine = origine;
 	}
-	void defEchelle(Vecteur3i echelle){
-		this->echelle = echelle;
+	void defEchelle(float echelle){
+		echelled = echelle;
+		surface->h = surface->h*echelled;
+		surface->w = surface->w*echelled;
 	}
 	Vecteur3f obtPosition(){ return position; }
 	Vecteur3f obtOrigine(){ return origine; }
 	Vecteur3f obtOrientation(){ return orientation; }
-	Vecteur3i obtEchelle(){ return echelle; }
+	Vecteur2i obtEchelle(){ return echelle; }
 };
