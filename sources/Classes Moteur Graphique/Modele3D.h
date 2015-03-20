1738289e0e8ece3a060e2139f5fe8ac6faa74db2
Kevin Pantelakis <kev.pantelakis@gmail.com>
2015-03-19 23:11:10 -0400

    Calcul des sommets modifié fini #10

--- sources/Classes Moteur Graphique/Modele3D.h
+++ sources/Classes Moteur Graphique/Modele3D.h
@@ -17,9 +17,23 @@ namespace gfx{
 	private:
 		Modele modele;
 		Texture texture;
-
+		float matTrans[16];
 		Vecteur3f echelle;
 		Vecteur3f boiteDeCollision[8];
+		
+		void obtMatriceTransformation(){
+			glPushMatrix();
+				glLoadIdentity();
+				glTranslatef(position.x - origine.x, position.y - origine.y, position.z - origine.z);
+				glRotatef(orientation.x, 1, 0, 0);
+				glRotatef(orientation.y, 0, 1, 0);
+				glRotatef(orientation.z, 0, 0, 1);
+				glTranslatef(origine.x, origine.y, origine.z);
+				glScalef(echelle.x, echelle.y, echelle.z);
+				glGetFloatv(GL_MODELVIEW_MATRIX, matTrans);
+			glPopMatrix();
+		}
+
 		void calculerBoiteDeCollision(){
 			std::vector<float> tmpX;
 			std::vector<float> tmpY;
@@ -78,6 +92,34 @@ namespace gfx{
 			echelle = Vecteur3f(1, 1, 1);
 		}
 
+		float* obtenirBoiteDeCollision(){}
+
+		float* obtommetsModifies(){
+			obtMatriceTransformation();
+			float* sommets = new float[modele.obtNbrVertices()];
+			float som[4];
+			float somTemp[4];
+			for (unsigned int i = 0; i < modele.obtNbrVertices() / 3; i++)
+			{
+				som[3] = 1;
+				for (unsigned int j = 0; j < 3; j++)
+					somTemp[j] = modele.obtVertices()[i * 3 + j];
+
+				som[0] = (double)(matTrans[0] * somTemp[0]) + (double)(matTrans[4] * somTemp[1]) + (double)(matTrans[8] * somTemp[2]) + (double)(matTrans[12] * somTemp[3]);
+				som[1] = (double)(matTrans[1] * somTemp[0]) + (double)(matTrans[5] * somTemp[1]) + (double)(matTrans[9] * somTemp[2]) + (double)(matTrans[13] * somTemp[3]);
+				som[2] = (double)(matTrans[2] * somTemp[0]) + (double)(matTrans[6] * somTemp[1]) + (double)(matTrans[10] * somTemp[2]) + (double)(matTrans[14] * somTemp[3]);
+				som[3] = (double)(matTrans[3] * somTemp[0]) + (double)(matTrans[7] * somTemp[1]) + (double)(matTrans[11] * somTemp[2]) + (double)(matTrans[15] * somTemp[3]);
+				
+				for (unsigned int j = 0; j < 3; j++)
+					sommets[i * 3 + j] = som[j] / som[3];
+				//sommets[i * 3 + 0] = som[0] / som[3];
+				//sommets[i * 3 + 1] = som[1] / som[3];
+				//sommets[i * 3 + 2] = som[2] / som[3];
+			}
+			return sommets;
+		}
+
+
 		~Modele3D(){}
 
 		void defModele(Modele &modele){
@@ -94,10 +136,6 @@ namespace gfx{
 
 		Modele obtModele(){
 			return modele;
-		}
-
-		Vecteur3f* obtBoiteDeCollision(){
-			return boiteDeCollision;
 		}
 
 		Texture obtTexture(){
@@ -154,23 +192,6 @@ namespace gfx{
 			position.z += axeZ;
 		}
 
-		/*To Do*/
-		void rafraichirSommets(){
-			/*
-			glPushMatrix();
-				glLoadIdentity();
-				glTranslatef(position.x - origine.x, position.y - origine.y, position.z - origine.z);
-				glRotatef(orientation.x, 1, 0, 0);
-				glRotatef(orientation.y, 0, 1, 0);
-				glRotatef(orientation.z, 0, 0, 1);
-				glTranslatef(origine.x, origine.y, origine.z);
-				glScalef(echelle.x, echelle.y, echelle.z);
-				float b[16];
-				glGetFloatv(GL_MODELVIEW_MATRIX, b);
-			glPopMatrix();
-			*/
-		}
-
 		void afficher(){
 			glBindTexture(GL_TEXTURE_2D, texture.obtID());
 			glPushMatrix();
@@ -187,5 +208,6 @@ namespace gfx{
 				glDrawArrays(GL_TRIANGLES, 0, modele.obtNbrFaces());
 			glPopMatrix();
 		}
+
 	};
 }
