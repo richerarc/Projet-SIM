b2153cdc98003fc6569b8584e65730585a483514
Dominique Fauteux-Chapleau <dominiquefauteuxchapleau@hotmail.com>
2015-03-23 11:21:53 -0400

    Retrait de classes inutiles

--- sources/Piege.h
+++
@@ -1,5 +0,0 @@
-class Piege : public Obstacle{
-public:
-	Piege(){}
-	Piege(gfx::Modele3D modele, unsigned int ID, char* materiaux) : Obstacle(modele, ID, materiaux){}
-};
--- sources/Puzzle.h
+++
@@ -1,6 +0,0 @@
-
-class Puzzle : public Obstacle{
-public:
-	Puzzle(){}
-	Puzzle(gfx::Modele3D modele, unsigned int ID, char* materiaux) : Obstacle(modele,ID,materiaux){}
-};
