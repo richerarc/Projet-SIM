dd4c4a70574c1d5765bbf88849779674410a151e
Kevin Pantelakis <kev.pantelakis@gmail.com>
2015-03-25 22:28:56 -0400

    ajoput du frustrum dans gestionaire 3d

--- sources/Gestionnaire3D.h
+++ sources/Gestionnaire3D.h
@@ -5,7 +5,7 @@ namespace gfx{
 	{
 	private:
 		std::list<Objet3D*> objets;
-		Camera3D Cam;
+		//Camera3D Cam;
 	public:
 		Gestionnaire3D(){
 		}
@@ -25,18 +25,27 @@ namespace gfx{
 		}
 
 		void vider(){
-			if (objets.empty == false){
+			if (!objets.empty()){
 				objets.clear();
 			}
 		}
 
-		void defCamera(Camera3D Camera){
+	//	void defCamera(Camera3D Camera){
 
-		}
+	//	}
 
 		~Gestionnaire3D(){
 			vider();
 		}
 
 	};
+
+	void defFrustrum(double angle,double ratio, double min, double max){
+		double HautY = Maths::degreARadian(angle);
+		double DroitX = HautY * ratio;
+		glMatrixMode(GL_PROJECTION); // Matrice de projection.
+		glLoadIdentity();
+		glFrustum(-DroitX, DroitX, -HautY, HautY, 1, 1000);
+		glMatrixMode(GL_MODELVIEW);
+	}
 }
