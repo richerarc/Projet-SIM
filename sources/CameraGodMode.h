a345a5142582f49f8c808c2a7f6a22eea6290898
SirKhepre <brandonroberge@hotmail.ca>
2015-03-30 23:46:33 -0400

    Finalisation de la caméra GodMode #12

--- sources/CameraGodMode.h
+++ sources/CameraGodMode.h
@@ -15,29 +15,32 @@ namespace gfx{
 		float vAngle;
 
 		void calculerVecteurs(){
-			/*if (vAngle > 90)
-				vAngle = 90;
-			if (vAngle < -90)
-				vAngle = -90;*/
+			if (vAngle > 89)
+				vAngle = 89;
+			if (vAngle < -89)
+				vAngle = -89;
 
 			devant.x = cos(vAngle * M_PI / 180) * cos(hAngle * M_PI / 180);
 			devant.y = cos(vAngle * M_PI / 180) * sin(hAngle * M_PI / 180);
 			devant.z = sin(vAngle * M_PI / 180);
 
+			devant.normaliser();
+
 			haut = Vecteur3f(0, 0, 1);
 
-			cote = haut.produitVectoriel(devant);
+			cote = devant.produitVectoriel(haut);
 			cote.normaliser();
 
-			cible = position + devant;
-			cible.normaliser();
-			//coustruireMatrice();
+			haut = cote.produitVectoriel(devant);
+			haut.normaliser();
+
+			coustruireMatrice();
 		}
 	public:
 
 		CameraGodMode(Vecteur3f& position) : Camera(position, Vecteur3f(), Vecteur3f(0, 0, 1)){
-			this->vitesse = 0.0001;
-			this->sensibilite = 0.002;
+			this->vitesse = 0.01;
+			this->sensibilite = 0.2;
 			
 			this->hAngle = 0;
 			this->vAngle = 0;
@@ -46,17 +49,19 @@ namespace gfx{
 
 			GestionnaireEvenements::obtInstance().ajouterUnRappel(SDL_MOUSEMOTION, std::bind(&CameraGodMode::surMouvementSouris, this, std::placeholders::_1));
 
+			SDL_SetRelativeMouseMode(SDL_TRUE);
 			SDL_ShowCursor(SDL_DISABLE);
 		}
 
 		~CameraGodMode(){
+			SDL_SetRelativeMouseMode(SDL_FALSE);
 			SDL_ShowCursor(SDL_ENABLE);
 		}
 
 		void surMouvementSouris(SDL_Event &event){
-			/*hAngle = event.motion.xrel * sensibilite;
-			vAngle = event.motion.yrel * sensibilite;
-			calculerVecteurs();*/
+			hAngle -= event.motion.xrel * sensibilite;
+			vAngle -= event.motion.yrel * sensibilite;
+			calculerVecteurs();
 		}
 
 		void deplacement(float frametime){
@@ -65,11 +70,16 @@ namespace gfx{
 			if (Clavier::toucheAppuyee(SDLK_s))
 				position -= devant * vitesse * frametime;
 			if (Clavier::toucheAppuyee(SDLK_a))
-				position += cote * vitesse * frametime;
-			if (Clavier::toucheAppuyee(SDLK_d))
 				position -= cote * vitesse * frametime;
-			
+			if (Clavier::toucheAppuyee(SDLK_d))
+				position += cote * vitesse * frametime;
+			if (Clavier::toucheAppuyee(SDLK_SPACE))
+				position.z += vitesse;
+			if (Clavier::toucheAppuyee(SDLK_LSHIFT))
+				position.z -= vitesse;
 		}
+
+		
 		
 	};
 }
