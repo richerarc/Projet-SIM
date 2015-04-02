6680425f6fb53fef970cab7f766ae2a5c572c4e9
Dominique Fauteux-Chapleau <dominiquefauteuxchapleau@hotmail.com>
2015-04-02 14:20:08 -0400

    Amuse toi Julien

--- sources/ObjetPhysique.h
+++ sources/ObjetPhysique.h
@@ -6,7 +6,7 @@
 class ObjetPhysique : public Objet{
 public:
 
-	ObjetPhysique(gfx::Modele3D modele, unsigned int ID, char* materiaux,double masse, Vecteur3d position) : Objet(modele, ID, materiaux, masse, Vecteur3d(0., 0., 0.), position){}
+	ObjetPhysique(gfx::Modele3D modele, unsigned int ID, char* materiaux,double masse, Vecteur3d vitesse, Vecteur3d position, Vecteur3d vitesseAngulaire, bool collisionInterne) : Objet(modele, ID, materiaux, masse, vitesse, position, vitesseAngulaire,collisionInterne){}
 
 	void appliquerPhysique(std::list<Objet*> objets) {
 		Physique::obtInstance().appliquerGravite(vitesse);
