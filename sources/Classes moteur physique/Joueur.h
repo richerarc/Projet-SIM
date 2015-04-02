4d9e120ddeee270d9cc6a95c06eb5e7db258cf5e
Nathan Boisjoli <nathan_boisjoli_7@hotmal.com>
2015-04-02 11:57:34 -0400

    Modification de la classe Joueur

--- sources/Classes moteur physique/Joueur.h
+++ sources/Classes moteur physique/Joueur.h
@@ -4,42 +4,22 @@
 
 class Joueur : public Objet{
 private:
-	double hauteur;
-	double largeur;
 	gfx::Modele3D modele3D;
 	bool accroupie;
 
 public:
-	Joueur(gfx::Modele3D &modele3D, unsigned int ID, double masse, Vecteur3d position, double largeur, double hauteur) : Objet(modele3D, ID, NULL, masse, NULL, position){
-		this->hauteur = hauteur;
-		this->largeur = largeur;
+	Joueur(gfx::Modele3D &modele3D, unsigned int ID, double masse, Vecteur3d position) : Objet(modele3D, ID, NULL, masse, NULL, position){
 		this->modele3D = modele3D;
 	}
 
-	void appliquerPhysique(Salle &salle, double frametime) {
+	void appliquerPhysique(Salle &salle, double &frametime) {
 		if (!Physique::obtInstance().collisionJoueurSalle(*this, salle)) {
 			Physique::obtInstance().appliquerGravite(vitesse);
 		}
 		defPosition(position + (vitesse * frametime));
 	}
 
-	double obtHauteur() {
-		return hauteur;
-	}
-
-	double obtLargeur() {
-		return largeur;
-	}
-
 	gfx::Modele3D obtModele3D() {
 		return modele3D;
-	}
-
-	void accroupie() {
-		hauteur = 1.00;
-	}
-
-	void debout() {
-		hauteur = 1.80;
 	}
 };
