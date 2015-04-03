f03c50750ea65d1c90515f23e4c418e1a9b8d2e6
Julien Dufresne <dufresne_julien@hotmail.ca>
2015-04-02 20:15:14 -0400

    Petite modification dans une procédure de physique

--- sources/Physique.h
+++ sources/Physique.h
@@ -426,20 +426,24 @@ public:
 
 		normale.normaliser();
 
-		if (fabs(normale.x) > fabs(normale.y) && fabs(normale.x) > fabs(normale.z)) {
+		double x = fabs(normale.x);
+		double y = fabs(normale.y);
+		double z = fabs(normale.z);
+
+		if (x > y && x > z) {
 
 			vect0 = Vecteur2d(v0.y, v0.z);
 			vect1 = Vecteur2d(v1.y, v1.z);
 			vect2 = Vecteur2d(v2.y, v2.z);
 		}
 
-		if (fabs(normale.y) > fabs(normale.x) && fabs(normale.y) > fabs(normale.z)) {
+		if (y > x && y > z) {
 
 			vect0 = Vecteur2d(v0.x, v0.z);
 			vect1 = Vecteur2d(v1.x, v1.z);
 			vect2 = Vecteur2d(v2.x, v2.z);
 		}
-		if (fabs(normale.z) > fabs(normale.x) && fabs(normale.z) > fabs(normale.y)) {
+		if (z > x && z > y) {
 
 			vect0 = Vecteur2d(v0.x, v0.y);
 			vect1 = Vecteur2d(v1.x, v1.y);
