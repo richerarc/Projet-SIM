9a1d99928fb85e463d71b8629e3602217d398088
Kevin Pantelakis <kev.pantelakis@gmail.com>
2015-04-07 13:15:36 -0400

    Edit connecteur

--- sources/Connecteur.h
+++ sources/Connecteur.h
@@ -1,12 +1,12 @@
 #pragma once	
 #include "Salle.h"
-class Connecteur : public Salle(){		
+class Connecteur : public Salle{
 private:
- 		 
+
 public:
 	Connecteur(){}
-	
-	Connecteur(Modele3D modele, int ID) : public Salle(modele, ID){}
- 		 
-	
+
+	Connecteur(gfx::Modele3D& modele, int ID, unsigned int nbrPorte) : Salle(modele, nbrPorte, ID){}
+
+
 };
