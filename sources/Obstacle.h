2bd992672738ddd1620fb909d9285c7a576c369c
Dominique Fauteux-Chapleau <dominiquefauteuxchapleau@hotmail.com>
2015-04-07 08:49:07 -0400

    Retrait de Obstacle.h

--- sources/Obstacle.h
+++
@@ -1,7 +0,0 @@
-#pragma once 
-#include "Objet.h"
-class Obstacle : public Objet{
-public:
-	Obstacle(){}
-	Obstacle(gfx::Modele3D& modele, unsigned int ID,char* materiaux,Vecteur3d position) : Objet(modele, ID, materiaux, 0, Vecteur3d(0.,0.,0.),position){}
-};
