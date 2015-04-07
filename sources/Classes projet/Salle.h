8c743d048cb994d32ac8e5b1674135b1a351f7fb
Gabriel Beaudry <gabriel.bdry@gmail.com>
2015-04-07 19:01:51 -0400

    Application de la physique #32

--- sources/Classes projet/Salle.h
+++ sources/Classes projet/Salle.h
@@ -1,4 +1,11 @@
 #pragma once
+#include "Physique.h"
+#include "Objet.h"
+#include "Vent.h"
+#include "Aimant.h"
+#include "ObjetFixe.h"
+#include "ObjetPhysique.h"
+
 class Salle{
 protected:
 	unsigned int ID;
@@ -12,6 +19,47 @@ public:
 		nbrPortes = nbrPorte;
 		this->ID = ID;
 		this->modele = modele;
+	}
+
+	void appliquerPhysiqueSurListeObjet(float frameTime) {
+		for (auto it : objets) {
+			if (it->obtMasse() != 0) {
+				Physique::obtInstance().appliquerGravite(it->obtVitesse(), frameTime);
+			}
+			Vent* it_Vent = dynamic_cast<Vent*>(it);
+			if (it_Vent != nullptr) {
+				for (auto it_Objet : objets) {
+					if (it_Objet->obtMasse() != 0) {
+						/*
+						if (it_Objet->obtModele3D().obtPosition().x >= it->obtPosition().x && it_Objet->obtModele3D().obtPosition().x <= it->obtPosition().x + it->obtDimensions().x) {
+						if (it_Objet->obtModele3D().obtPosition().y >= it->obtPosition().y && it_Objet->obtModele3D().obtPosition().y <= it->obtPosition().y + it->obtDimensions().y) {
+						if (it_Objet->obtModele3D().obtPosition().z >= it->obtPosition().z && it_Objet->obtModele3D().obtPosition().z <= it->obtPosition().z + it->obtDimensions().z) {
+						Physique::obtInstance().appliquerVent(it->obtVitesse(), *it_Objet, frameTime);
+						}
+						}
+						}
+						*/
+					}
+				}
+			}
+			Aimant* it_Aimant = dynamic_cast<Aimant*>(it);
+			if (it_Aimant != nullptr) {
+				for (auto it_Objet : objets) {
+					if (it_Objet->obtMasse() != 0) {
+						Physique::obtInstance().appliquerMagnetisme(*it_Objet, it_Aimant->obtPosition(), it_Aimant->obtForce(), frameTime);
+					}
+				}
+			}
+			ObjetFixe* it_ObjetFixe = dynamic_cast<ObjetFixe*>(it);
+			if (it_ObjetFixe != nullptr) {
+				for (auto it_Objet : objets) {
+					if (it_Objet->obtMasse() != 0) {
+						// collisions
+
+					}
+				}
+			}
+		}
 	}
 
 	unsigned int obtNbrPortes(){
