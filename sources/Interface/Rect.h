271d7ea4e044e8d4895917f2c043d1d633efab73
SirKhepre <brandonroberge@hotmail.ca>
2015-04-14 16:41:16 -0400

    Ajout de la classe Rect #34

---
+++ sources/Interface/Rect.h
@@ -0,0 +1,32 @@
+#pragma once 
+#include "Vecteur2.h"
+
+template<typename T>
+class Rect{
+public:
+	Rect(T x = 0, T y = 0, T l = 0, T h = 0){
+		this->h = h;
+		this->l = l;
+		this->y = y;
+		this->x = x;
+	}
+
+	bool contient(float x, float y){
+		bool estDansX = x >= this->x && x <= (this->x + this->l);
+		bool estDansY = y >= this->y && y <= (this->y + this->h);
+		return estDansX && estDansY;
+	}
+	bool contient(Vecteur2<T> &point){
+		return contains(point.x, point.y);
+	}
+	bool intersecte(Rect<T> &rect){
+		if (contient(rect.x, rect.y) || contient(rect.x, rect.y + rect.h) || contient(rect.x + rect.l, rect.y) || contient(rect.x + rect.l, rect.y + rect.h))
+			return true;
+		return false;
+	}
+
+	T x, y, l, h;
+};
+typedef Rect<int> Recti;
+typedef Rect<float> Rectf;
+typedef Rect<double> Rectd;
