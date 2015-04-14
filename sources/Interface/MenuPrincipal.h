64f8198fe19e971b46ca679f578c02003b81e1be
Xavier St-Georges <xavierst-georges@hotmail.com>
2015-04-14 17:44:06 -0400

    Menu Principale Fini #34

--- sources/Interface/MenuPrincipal.h
+++ sources/Interface/MenuPrincipal.h
@@ -2,67 +2,47 @@
 #include "Menu.h"
 #include "Sprite2D.h"
 #include "GestionnairePhases.h"
+#include "Jeu.h"
 
 
 class MenuPrincipal : public Menu {
-
 private:
-	Bouton* NouvellePartie;
-	Bouton* Options;
-
+	gfx::Texte2D* nouvellePartie;
+	gfx::Texte2D* options;
+	gfx::Sprite2D* logo;
 public:
-	MenuPrincipal(void) : Menu() {
-		
-		this->SpriteFond = new gfx::Sprite2D(Vecteur2f(0, 0), &gfx::GestionnaireRessources::obtInstance().obtTexture("C:\\Users\\Xavier\\Desktop\\cube.png"));
+	MenuPrincipal(void) {
+		nouvellePartie = new gfx::Texte2D("Nouvelle Partie", "arial.ttf", 9, Vecteur2f(150, 400));
+		options = new gfx::Texte2D("Options", "arial.ttf", 9, Vecteur2f(150, 200));
 
-		gfx::Texture* TextureSpriteBNPFond = &gfx::GestionnaireRessources::obtInstance().obtTexture("C:\\Users\\Xavier\\Desktop\\blanc.png");
+		logo = new gfx::Sprite2D(Vecteur2f(150, 400), &gfx::GestionnaireRessources::obtInstance().obtTexture(""));
+		this->Retour = new gfx::Texte2D("Retour", "arial.ttf", 15, Vecteur2f(150, 0));
 
-		gfx::Texture* TextureSpriteBNPClique = new gfx::Texture();
-		gfx::GestionnaireRessources::obtInstance().chargerTexture("C:\\Users\\Xavier\\Desktop\\rose.png");
+		this->SpriteFond = new gfx::Sprite2D(Vecteur2f(150, 400), &gfx::GestionnaireRessources::obtInstance().obtTexture("")),
 
-		gfx::Texture* TextureSpriteBNPSurvol = new gfx::Texture();
-		gfx::GestionnaireRessources::obtInstance().chargerTexture("C:\\Users\\Xavier\\Desktop\\turquoise.png");
-		NouvellePartie = new Bouton(Vecteur2f(150, 400),Vecteur2f(300, 150),
-			new gfx::Texte2D("Nouvelle Partie", "arial.ttf", 9, Vecteur2f(150, 400)),
-			new gfx::Sprite2D(Vecteur2f(150, 400), TextureSpriteBNPFond),
-			new gfx::Sprite2D(Vecteur2f(150, 400), TextureSpriteBNPClique),
-			new gfx::Sprite2D(Vecteur2f(150, 400), TextureSpriteBNPSurvol));
-
-
-
-		Options = new Bouton(Vecteur2f(150, 200), Vecteur2f(300, 150),
-			new gfx::Texte2D("Options", "arial.ttf", 9, Vecteur2f(150, 200)),
-			new gfx::Sprite2D(Vecteur2f(150, 200), TextureSpriteBNPFond),
-			new gfx::Sprite2D(Vecteur2f(150, 200), TextureSpriteBNPClique),
-			new gfx::Sprite2D(Vecteur2f(150, 200), TextureSpriteBNPSurvol));
-
-		this->Titre = new Etiquette( new gfx::Texte2D("The Journalist", "arial.ttf", 20, Vecteur2f(150, 300)), new gfx::Sprite2D(Vecteur2f(150,300),TextureSpriteBNPFond),Vecteur2f(150,300),Vecteur2f(300, 300));
-
-		this->Retour = new Bouton(Vecteur2f(150, 0), Vecteur2f(300, 150), new gfx::Texte2D("Retour", "arial.ttf", 15, Vecteur2f(150, 0)), new gfx::Sprite2D(Vecteur2f(150, 0), TextureSpriteBNPFond),  new gfx::Sprite2D(Vecteur2f(150, 0), TextureSpriteBNPClique), new gfx::Sprite2D(Vecteur2f(150, 0), TextureSpriteBNPSurvol));
-
-	}
-	bool obtActif(void) {
-		return this->Actif;
-	}
-	
-	void defActif(bool Actif) {
-		this->Actif = Actif;
+		gfx::Gestionnaire2D::obtInstance().ajouterObjet(nouvellePartie);
+		gfx::Gestionnaire2D::obtInstance().ajouterObjet(options);
+		gfx::Gestionnaire2D::obtInstance().ajouterObjet(this->Retour);
+		gfx::Gestionnaire2D::obtInstance().ajouterObjet(logo);
+		gfx::Gestionnaire2D::obtInstance().ajouterObjet(this->SpriteFond);
 	}
 
-	void actualiser(){
-		if (NouvellePartie->enClique()){
+	~MenuPrincipal(void) {
+		gfx::Gestionnaire2D::obtInstance().vider();
+	}
+
+	void actualiser(void) {
+		if (nouvellePartie->obtRectangle().contient(Souris::obtPosition().x, Souris::obtPosition().y)) {
 			GestionnairePhases::obtInstance().retirerPhase();
 			GestionnairePhases::obtInstance().ajouterPhase(new PhaseNouvellePartie());
 		}
+		else if (options->obtRectangle().contient(Souris::obtPosition().x, Souris::obtPosition().y)) {
+			GestionnairePhases::obtInstance().retirerPhase();
+			GestionnairePhases::obtInstance().ajouterPhase(new PhaseOptions());
+		}
+		else if (this->Retour->obtRectangle().contient(Souris::obtPosition().x, Souris::obtPosition().y)) {
+			Jeu::fenetre->fermer();
+		}
 	}
-
-	void afficher(void) {
-		gfx::Gestionnaire2D::obtInstance().ajouterObjet(this->SpriteFond);
-		NouvellePartie->afficher();
-		Options->afficher();
-		this->Titre->afficher();
-		this->Retour->afficher();
-	}
-
 
 };
