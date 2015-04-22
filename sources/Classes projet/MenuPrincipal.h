513c49edd8e32fea91162153d4f65090405aacdc
Kevin Pantelakis <kev.pantelakis@gmail.com>
2015-04-21 23:24:43 -0400

    Revert "Finition de la class MenuPrincipal."
    This reverts commit be5b93e515a75616cf84fdcfd52e333905c92b4d.

--- sources/Classes projet/MenuPrincipal.h
+++ sources/Classes projet/MenuPrincipal.h
@@ -3,71 +3,73 @@
 #include "Sprite2D.h"
 #include "GestionnairePhases.h"
 #include "Jeu.h"
+#include "Texte2D.h"
 #include "PhaseMenuNouvellePartie.h"
 #include "PhaseMenuOptions.h"
 #include "Glisseur.h"
-#include "Bouton.h"
 
 
 class MenuPrincipal : public Menu {
 private:
-	Bouton* demarrer;
-	Bouton* options;
-	Bouton* quitter;
+	gfx::Texte2D* nouvellePartie;
+	gfx::Texte2D* options;
 	gfx::Sprite2D* logo;
 public:
 	MenuPrincipal(void) {
-		spriteFond = new gfx::Sprite2D(Vecteur2f(0, 0), &gfx::GestionnaireRessources::obtInstance().obtTexture("fondMenu.png"));
-		gfx::Gestionnaire2D::obtInstance().ajouterObjet(spriteFond);
+		nouvellePartie = new gfx::Texte2D("New Game", gfx::GestionnaireRessources::obtInstance().obtPolice("arial.ttf", "arial45", 50), Vecteur2f(50, 330));
+		options = new gfx::Texte2D("Options", gfx::GestionnaireRessources::obtInstance().obtPolice("arial.ttf", "arial45", 50), Vecteur2f(50, 180));
 
-		demarrer = new Bouton(std::bind(&MenuPrincipal::enClicDemarrer, this, std::placeholders::_1), 
-							  std::bind(&MenuPrincipal::survol, this, std::placeholders::_1), 
-							  std::bind(&MenuPrincipal::defaut, this, std::placeholders::_1), 
-							  Vecteur2f(100, 260),
-							  "New Game");
-		options = new Bouton(std::bind(&MenuPrincipal::enClicOptions, this, std::placeholders::_1),
-							 std::bind(&MenuPrincipal::survol, this, std::placeholders::_1),
-			                 std::bind(&MenuPrincipal::defaut, this, std::placeholders::_1),
-			                 Vecteur2f(100, 235),
-			                 "Options");
-		quitter = new Bouton(std::bind(&MenuPrincipal::enClicQuitter, this, std::placeholders::_1),
-							 std::bind(&MenuPrincipal::survol, this, std::placeholders::_1),
-							 std::bind(&MenuPrincipal::defaut, this, std::placeholders::_1),
-							 Vecteur2f(100, 210),
-							 "Quit");
-		logo = new gfx::Sprite2D(Vecteur2f(100, 350), &gfx::GestionnaireRessources::obtInstance().obtTexture("logo.png"));
-		gfx::Gestionnaire2D::obtInstance().ajouterObjet(logo);
+		logo = new gfx::Sprite2D(Vecteur2f(160, 500), &gfx::GestionnaireRessources::obtInstance().obtTexture("logo.png"));
+		this->Retour = new gfx::Texte2D("Quit", gfx::GestionnaireRessources::obtInstance().obtPolice("arial.ttf", "arial50", 50), Vecteur2f(50, 30));
+
+		this->SpriteFond = new gfx::Sprite2D(Vecteur2f(000, 000), &gfx::GestionnaireRessources::obtInstance().obtTexture("Joueur.png"));
+
+		gfx::Gestionnaire2D::obtInstance().ajouterObjets({ this->SpriteFond, logo, nouvellePartie, options, this->Retour });
+		
 	}
 
 	~MenuPrincipal(void) {
-		gfx::Gestionnaire2D::obtInstance().retObjets({ logo, spriteFond });
-		delete logo, spriteFond;
-	}
-
-	void survol(Bouton* sender){
-		sender->defCouleur({ 255, 0, 0, 255 });
-	}
-
-	void defaut(Bouton* sender){
-		sender->defCouleur({ 0, 0, 0, 255 });
-	}
-
-	void enClicDemarrer(Bouton* sender){
-		GestionnairePhases::obtInstance().retirerPhase();
-		//GestionnairePhases::obtInstance().ajouterPhase(new PhaseMenuNouvellePartie());
-	}
-
-	void enClicOptions(Bouton* sender){
-		GestionnairePhases::obtInstance().retirerPhase();
-		//GestionnairePhases::obtInstance().ajouterPhase(new PhaseMenuOptions());
-	}
-
-	void enClicQuitter(Bouton* sender){
-		GestionnairePhases::obtInstance().retirerPhase();
+		gfx::Gestionnaire2D::obtInstance().vider();
 	}
 
 	void actualiser(void) {
 
+		if (nouvellePartie->obtRectangle().contient(Souris::obtPosition().x, Souris::obtPosition().y)) {
+			nouvellePartie->defCouleur({ 215, 110, 75, 255 });
+			if (Souris::boutonAppuye(SDL_BUTTON_LEFT)){
+				GestionnairePhases::obtInstance().retirerPhase();
+				gfx::Gestionnaire2D::obtInstance().vider();
+				GestionnairePhases::obtInstance().ajouterPhase(new PhaseMenuNouvellePartie());
+			}
+		}
+		else{
+			nouvellePartie->defCouleur({ 0, 0, 0, 255 });
+		}
+		if (options->obtRectangle().contient(Souris::obtPosition().x, Souris::obtPosition().y)) {
+			options->defCouleur({ 215, 110, 75, 255 });
+			if (Souris::boutonAppuye(SDL_BUTTON_LEFT)){
+				GestionnairePhases::obtInstance().obtDerniere()->defPause(true);
+				gfx::Gestionnaire2D::obtInstance().vider();
+				GestionnairePhases::obtInstance().ajouterPhase(new PhaseMenuOptions());
+			}
+		}
+		else{
+			options->defCouleur({ 0, 0, 0, 255 });
+		}
+		if (this->Retour->obtRectangle().contient(Souris::obtPosition().x, Souris::obtPosition().y)) {
+			this->Retour->defCouleur({ 215, 110, 75, 255 });
+			if (Souris::boutonAppuye(SDL_BUTTON_LEFT)){
+				GestionnairePhases::obtInstance().retirerPhase();
+			}
+		}
+		else{
+			this->Retour->defCouleur({ 0, 0, 0, 255 });
+		}
+	}
+
+	void remplir() {
+
+		gfx::Gestionnaire2D::obtInstance().ajouterObjets({ this->SpriteFond, logo, nouvellePartie, options, this->Retour });
 	}
 
 };
