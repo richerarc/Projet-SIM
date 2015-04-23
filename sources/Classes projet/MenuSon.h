fbcb5b6281a91bb742570e1901153981d6045cc1
Julien Dufresne <dufresne_julien@hotmail.ca>
2015-04-23 11:49:22 -0400

    Phases

--- sources/Classes projet/MenuSon.h
+++ sources/Classes projet/MenuSon.h
@@ -15,15 +15,24 @@ public:
 
 	MenuSon(void) : Menu() {
 
-		
+		spriteFond = new gfx::Sprite2D(Vecteur2f(0, 0), &gfx::GestionnaireRessources::obtInstance().obtTexture("fondMenu.png"));
+		volumeJeu = new gfx::Texte2D("Game sound", gfx::GestionnaireRessources::obtInstance().obtPolice("arial.ttf", "arial50", 50), Vecteur2f(100, 500));
+
+		volume = new Glisseur(Vecteur2f(100, 400), Vecteur2f(1,1), 50);
 
 	}
 
 	void remplir(void) {
 
-		
+		gfx::Gestionnaire2D::obtInstance().ajouterObjets({ spriteFond, volumeJeu });
+		volume->remplir();
 
 	}
 
-	void defPause(bool pause) {}
+	void defPause(bool pause) {
+
+
+
+	}
+
 };
