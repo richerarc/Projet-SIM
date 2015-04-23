fbf7f4143b4bda60952ac73ebb6199ff242047ca
Patrick Singcaster <singcaster@me.com>
2015-04-22 21:47:47 -0400

    Ajout de commande préprocesseur pour l'inclusion des librairies nécessaires pour Win32

--- sources/Classes projet/main.cpp
+++ sources/Classes projet/main.cpp
@@ -5,6 +5,14 @@
 #include <SDL2_ttf/SDL_ttf.h>
 #include <SDL2/SDL_opengl.h>
 #elif defined _WIN32
+#pragma comment ( lib,"opengl32.lib")
+#pragma comment ( lib,"SDL2main.lib")
+#pragma comment ( lib,"SDL2.lib")
+#pragma comment ( lib,"SDL2_image.lib")
+#pragma comment ( lib,"SDL2_ttf.lib")
+#pragma comment ( lib,"SDL2_mixer.lib")
+#pragma comment(linker, "/SUBSYSTEM:windows /ENTRY:mainCRTStartup")
+#pragma comment(linker, "/NODEFAULTLIB:msvcrt.lib")
 #include <SDL2\SDL.h>
 #include <SDL2\SDL_image.h>
 #include <SDL2\SDL_ttf.h>
