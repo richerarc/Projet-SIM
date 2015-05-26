d2346dfdba10abe0dc6084bcf60df06d361a1130
Nicolas Dean <Gody117@hotmail.com>
2015-05-26 15:16:57 -0400

    Mini modif sur note et journal

--- sources/Classes projet/Journal.h
+++ sources/Classes projet/Journal.h
@@ -1,18 +1,15 @@
 #pragma once
 #include "Item.h"
-#include "Texte2D.h"
 
 class Journal : public Item {
 
 private:
 
-	gfx::Texte2D* code;
-
 public:
 
 	Journal(int type, char* nom, char* description, char* cheminIcone, gfx::Modele3D* modele, unsigned int ID, char* materiaux, double masse, gfx::Texte2D* code) : Item(type, nom, description, cheminIcone, 1, modele, 20, materiaux, masse){
 		
-		this->code = new gfx::Texte2D();
+		
 
 	}
 
@@ -21,11 +18,7 @@ public:
 	}
 
 	void equiper(){
-
-	}
-
-	void defcode(std::string* texte) {
-		code->defTexte(texte);
+		
 	}
 
 };
