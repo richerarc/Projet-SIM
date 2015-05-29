7bc01dd177697f377903ba30defc9c7112b3375d
richerarc <richer.arc@gmail.com>
2015-05-29 08:38:04 -0400

    Carte fonctionnelle

--- sources/Classes projet/Carte.h
+++ sources/Classes projet/Carte.h
@@ -1264,7 +1264,8 @@ public:
 			}
 		}
 		
-		ajouterLien(Entree(IDsalle, IDporte, false), Sortie(salleTeleporteur.ID, 0));
+		ajouterLien(Entree(salleTeleporteur.ID, 0, false), Sortie(it->ID, IDporte));
+		ajouterLien(Entree(it->ID, IDporte, false), Sortie(salleTeleporteur.ID, 0));
 		
 		infosSalles.push_back(salleTeleporteur);
 	}
@@ -1319,6 +1320,28 @@ public:
 		avion.type = 180;
 		
 		salleFin.Objet.push_back(avion);
+		
+		int IDsalle, IDporte;
+		
+		InfoObjet obj;
+		obj.largeur = 0;
+		LecteurFichier::lireObjet("Ressources/Info/portePlate.txt", obj);
+		gfx::Modele3D* mod;
+		for (auto &it : infosSalles){
+			if (it.nbrPorte < 3){
+				IDsalle = it.ID;
+				IDporte = obj.ID = it.Objet.size();
+				mod = new gfx::Modele3D(gfx::GestionnaireRessources::obtInstance().obtModele(it.cheminModele), gfx::GestionnaireRessources::obtInstance().obtTexture(it.cheminTexture));
+				mod->defEchelle(it.echelle.x, it.echelle.y, it.echelle.z);
+				it.nbrPorte++;
+				positionnerPorte(*mod, it, obj);
+				it.Objet.push_back(obj);
+				break;
+			}
+		}
+		
+		ajouterLien(Entree(salleFin.ID, 0, false), Sortie(it->ID, IDporte));
+		ajouterLien(Entree(it->ID, IDporte, false), Sortie(salleFin.ID, 0));
 		
 		infosSalles.push_back(salleFin);
 	}
