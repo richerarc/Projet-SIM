92679c0bebc9d86f5acb8ef44cf65e23b0d2e17a
Nicolas Dean <Gody117@hotmail.com>
2015-05-31 20:26:37 -0400

    Faire compiler le projet

--- sources/Classes projet/PlafondTueur.h
+++ sources/Classes projet/PlafondTueur.h
@@ -3,10 +3,17 @@
 
 class PlafondTueur : public ObjetFixe{
 private:
-	
+	Vecteur3d posPlafInitiale;
 public:
 	PlafondTueur(gfx::Modele3D* modele, unsigned int ID, char* materiaux, Vecteur3d position, Vecteur3d vitesseAngulaire, bool collisionInterne, bool interactif) : ObjetFixe(modele, ID, materiaux, position, vitesseAngulaire, collisionInterne, interactif){
 		this->porte = false;
+		this->posPlafInitiale = position;
+	}
+
+	Vecteur3d obtPositionInitiale(){
+
+		return posPlafInitiale;
+
 	}
 
 
